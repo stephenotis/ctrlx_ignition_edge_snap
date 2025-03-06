e416019529b9f5e610d66ec34bb8a1d96f27891d
stephenotis <sotis85@gmail.com>
2025-03-05 16:34:06 -0800

    Add files via upload

---
+++ build_snap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET_ARCH=$1
+echo TARGET_ARCH: ${TARGET_ARCH}
+echo --- clean snap
+snapcraft clean --destructive-mode
+echo --- build snap with architecture ${TARGET_ARCH}
+snapcraft --destructive-mode --enable-experimental-target-arch --target-arch=${TARGET_ARCH}
+echo --- clean snap
+snapcraft clean --destructive-mode
